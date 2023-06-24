22211c573b970f93018eb9b463223bfdd44541c8
Ashleen <2552805d@student.gla.ac.uk>
2023-06-24 18:17:08 +0100

    added repositories to fastapi state

--- src/app.py
+++ src/app.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
 from src.routes import room
-from src.repository.instance import roomRepository
+from src.repository.instance import roomRepository, incidentRepository, playerRepository, gameRepository
 
 app = FastAPI(title="Bingo API", docs_url="/")
 app.include_router(room.router, prefix="/room", tags=["room"])
 app.state.roomRepository = roomRepository
+app.state.incidentRepository = incidentRepository
+app.state.playerRepository = playerRepository
+app.state.gameRepository = gameRepository
